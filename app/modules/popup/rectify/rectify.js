0a53570cbd2f2da87373e88a299e2231e9c6a92b
Dmitrii Shchadei <gmetrofun@gmail.com>
2013-07-22 11:43:48 +0300

    added emoji

--- app/modules/popup/rectify/rectify.js
+++ app/modules/popup/rectify/rectify.js
@@ -1,31 +1,38 @@
-/*global linkify */
+/*global jEmoji*/
 angular.module(
     'rectify', ['i18n']
 ).filter('rectify', function ($filter) {
-    var MAX_TEXT_LENGTH = 300,
-        TRUNCATE_LENGTH = 200,
+    var MAX_TEXT_LENGTH = 3,
+        TRUNCATE_LENGTH = 2,
 
         label = $filter('i18n')('more...');
 
     jQuery('body').on('click', '.show-more', function (e) {
         var jTarget = jQuery(e.currentTarget);
 
-        jTarget.replaceWith(linkify(jTarget.data('text')));
+        console.log(jTarget.data('emoji'));
+        jTarget.replaceWith(linkifyAndEmoji(
+            jTarget.data('text'),
+            jTarget.data('emoji') === 'yes'
+        ));
     });
 
     /**
-     * Also replaces next wiki format: [id12345|Dmitrii],
+     * Replaces next wiki format: [id12345|Dmitrii],
      * or [club32194285|Читать прoдoлжение..]
      * with <a anchor="http://vk.com/id12345">Dmitrii</a>
+     * And repaces emoji unicodes with corrspondenting images
      *
      * @param {String} text
      * @returns {String} html
      */
-    function linkify(text) {
-        return text.replace(
+    function linkifyAndEmoji(text, hasEmoji) {
+        var linkifiedText = text.replace(
             /\[((?:id|club)\d+)\|([^\]"']+)\]/g,
             '<a anchor="http://vk.com/$1">$2</a>'
         );
+
+        return hasEmoji ? jEmoji.unifiedToHTML(linkifiedText):linkifiedText;
     }
 
     function escapeQuotes(string) {
@@ -40,14 +47,16 @@ angular.module(
     }
     /**
      * Truncates long text, and add pseudo-link "show-more"
-     * Also replaces next wiki format: [id12345|Dmitrii]
+     * Replaces next wiki format: [id12345|Dmitrii]
      * with <a anchor="http://vk.com/id12345">Dmitrii</a>
+     * And repaces emoji unicodes with corrspondenting images
      *
      * @param {String} text
+     * @param {Boolean} hasEmoji If true, then we need to replace missing unicodes with images
      *
      * @returns {String} html-string
      */
-    return function (text) {
+    return function (text, hasEmoji) {
         var spaceIndex;
 
         if (text) {
@@ -56,16 +65,17 @@ angular.module(
                 spaceIndex = text.indexOf(' ', TRUNCATE_LENGTH);
 
                 if (spaceIndex !== -1) {
-                    return  linkify(text.slice(0, spaceIndex)) + [
+                    return  linkifyAndEmoji(text.slice(0, spaceIndex), hasEmoji) + [
                         ' <span class="show-more btn btn-mini" data-text="',
-                        escapeQuotes(text.slice(spaceIndex)),
-                        '" type="button">', label, '</span>'
+                        escapeQuotes(text.slice(spaceIndex)), '" ',
+                        hasEmoji ? 'data-emoji="yes" ':'',
+                        'type="button">', label, '</span>'
                     ].join('');
                 } else {
-                    return linkify(text);
+                    return linkifyAndEmoji(text, hasEmoji);
                 }
             } else {
-                return linkify(text);
+                return linkifyAndEmoji(text, hasEmoji);
             }
         }
     };
