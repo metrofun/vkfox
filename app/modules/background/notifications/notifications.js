29ebfe831576cd157a3396a48f55da29a40e3fba
Dmitrii Shchadei <gmetrofun@gmail.com>
2013-08-29 00:18:05 +0300

    Implemented settings system(including Audio)

--- app/modules/background/notifications/notifications.js
+++ app/modules/background/notifications/notifications.js
@@ -1,7 +1,75 @@
-angular.module('notifications', ['mediator'])
-    .config(function ($provide) {
+angular.module('notifications', ['mediator', 'persistent-model'])
+    .constant('STANDART_SIGNAL', 'audio/standart.mp3')
+    .constant('ORIGINAL_SIGNAL', 'audio/original.mp3')
+    .factory('notificationSettings', function (Mediator, PersistentModel, STANDART_SIGNAL) {
+        var notificationSettings = new PersistentModel(
+            {
+                enabled: true,
+                sound: {
+                    enabled: true,
+                    volume: 0.5,
+                    signal: STANDART_SIGNAL
+                },
+                popups: {
+                    enabled: true,
+                    showText: true
+                }
+            },
+            {name: 'notificationSettings'}
+        );
+
+        Mediator.sub('notifications:settings:get', function () {
+            Mediator.pub('notifications:settings', notificationSettings.toJSON());
+        });
+        Mediator.sub('notifications:settings:put', function (settings) {
+            notificationSettings.set(settings);
+        });
+
+        return notificationSettings;
+    })
+    .factory('notificationQueue', function (notificationSettings, Mediator) {
+        var notificationQueue = new Backbone.Collection();
+
+        chrome.browserAction.setBadgeBackgroundColor({
+            color: [231, 76, 60, 255]
+        });
+        notificationSettings.on('change:enabled', function (event, enabled) {
+            var count = notificationQueue.size();
+
+            chrome.browserAction.setBadgeText({
+                text: (enabled && count && String(count)) || ''
+            });
+        });
+        notificationQueue.on('add remove reset', function () {
+            if (notificationSettings.get('enabled')) {
+                var count = notificationQueue.size();
+
+                chrome.browserAction.setBadgeText({
+                    text: count ? String(count):''
+                });
+            }
+        });
+        Mediator.sub('auth:success', function () {
+            notificationQueue.reset();
+        });
+        // Remove seen updates
+        Mediator.sub('router:change', function (params) {
+            if (params.tab && notificationQueue.size()) {
+                notificationQueue.remove(notificationQueue.where({
+                    type: params.tab
+                }));
+            }
+        });
+        Mediator.sub('notifications:queue:get', function () {
+            Mediator.pub('notifications:queue', notificationQueue.toJSON());
+        });
+
+        return notificationQueue;
+    })
+    .factory('Notifications', function (notificationQueue, notificationSettings) {
         var QUEUE_TYPES = ['chat', 'news'],
-            notificationQueue = new Backbone.Collection();
+            audioInProgress = false,
+            audio = new Audio();
 
         function getBase64FromImage(url, onSuccess, onError) {
             var xhr = new XMLHttpRequest();
@@ -31,60 +99,51 @@ angular.module('notifications', ['mediator'])
             xhr.send();
         }
 
-        chrome.browserAction.setBadgeBackgroundColor({
-            color: [231, 76, 60, 255]
-        });
-        notificationQueue.on('add remove reset', function () {
-            var count = notificationQueue.size();
+        return {
+            /**
+            * Show new notifications
+            *
+            * @param {Object} options
+            * @param {String} options.title
+            * @param {String} [options.photo]
+            * @param {String} [options.message='']
+            */
+            create: function (type, options) {
+                var popups = notificationSettings.get('popups').enabled,
+                    sound = notificationSettings.get('sound');
 
-            chrome.browserAction.setBadgeText({
-                text: count ? String(count):''
-            });
-        });
-
-        $provide.value('notificationQueue', notificationQueue);
-        $provide.factory('Notifications', function () {
-            return {
-                /**
-                * Show new notifications
-                *
-                * @param {Object} options
-                * @param {String} options.title
-                * @param {String} [options.photo]
-                * @param {String} [options.message='']
-                */
-                create: function (type, options) {
-                    if (QUEUE_TYPES.indexOf(type) !== -1) {
-                        notificationQueue.push({type: type});
-                    }
-
-                    // TODO on error
-                    getBase64FromImage(options.image, function (base64) {
-                        chrome.notifications.create(_.uniqueId(), {
-                            type: 'basic',
-                            title: options.title,
-                            message: options.message || '',
-                            iconUrl: base64
-                        }, function () {});
-                    });
+                if (QUEUE_TYPES.indexOf(type) !== -1) {
+                    notificationQueue.push({type: type});
                 }
-            };
-        });
-    })
-    .run(function (Mediator, notificationQueue) {
-        Mediator.sub('auth:success', function () {
-            notificationQueue.reset();
-        });
-        // Remove seen updates
-        Mediator.sub('router:change', function (params) {
-            if (params.tab && notificationQueue.size()) {
-                notificationQueue.remove(notificationQueue.where({
-                    type: params.tab
-                }));
-            }
-        });
-        Mediator.sub('notifications:queue:get', function () {
-            Mediator.pub('notifications:queue', notificationQueue.toJSON());
-        });
 
+                if (notificationSettings.get('enabled')) {
+                    if (popups.enabled) {
+                        // TODO on error
+                        getBase64FromImage(options.image, function (base64) {
+                            chrome.notifications.create(_.uniqueId(), {
+                                type: 'basic',
+                                title: options.title,
+                                message: (popups.showText && options.message) || '',
+                                iconUrl: base64
+                            }, function () {});
+                        });
+                    }
+                    console.log(sound.enabled, audioInProgress);
+                    if (sound.enabled && !audioInProgress) {
+                        audioInProgress = true;
+
+                        audio.volume = sound.volume;
+                        audio.src = sound.signal;
+                        audio.play();
+
+                        audio.addEventListener('ended', function () {
+                            audioInProgress = false;
+                        });
+                    }
+                }
+            }
+        };
+    })
+    .run(function (notificationQueue) {
+        notificationQueue.reset();
     });
--- app/modules/popup/settings/settings.js
+++ app/modules/popup/settings/settings.js
@@ -1,3 +1,23 @@
 angular.module('settings', [])
-    .controller('settingsCtrl', function ($scope, Mediator) {
+    .controller('settingsNotificationsCtrl', function ($scope, Mediator) {
+        Mediator.sub('notifications:settings', function (settings) {
+            $scope.$apply(function () {
+                $scope.notifications = settings;
+            });
+
+            $scope.$watch('notifications', function (settings) {
+                Mediator.pub('notifications:settings:put', settings);
+            }, true);
+            $scope.$watchCollection('notifications.sound', _.debounce(function () {
+                var audio = new Audio(),
+                    sound = $scope.notifications.sound;
+
+                audio.volume = sound.volume;
+                audio.src = sound.signal;
+                audio.play();
+                audio.addEventListener('ended', function () {
+                });
+            }, 300), true);
+        });
+        Mediator.pub('notifications:settings:get');
     });
