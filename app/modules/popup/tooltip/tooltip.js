cd942372550c2061eacad1ef3178b315f8a592bb
Dmitrii Shchadei <gmetrofun@gmail.com>
2013-09-14 18:29:52 +0300

    Fixed tooltip positioning: fixed running of the screen

--- app/modules/popup/tooltip/tooltip.js
+++ app/modules/popup/tooltip/tooltip.js
@@ -2,7 +2,21 @@ angular.module('tooltip', []).run(function () {
     jQuery('body').tooltip({
         selector: '[title]',
         delay: { show: 1000, hide: false},
-        placement: 'bottom'
+        placement: function (tooltip) {
+            setTimeout(function () {
+                var $tooltip = jQuery(tooltip),
+                    $inner = jQuery('.tooltip-inner', tooltip),
+                    offset = jQuery(window).width() - $tooltip.offset().left - $tooltip.width();
+
+                if (offset < 0) {
+                    $inner.css({
+                        'position': 'relative',
+                        'left':  offset + 'px'
+                    });
+                }
+            });
+            return 'bottom';
+        }
     });
 
     // Hide popup on click
