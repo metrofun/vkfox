5e06b4369411f68eadbdee04a31696e8404d665f
Dmitrii Shchadei <gmetrofun@gmail.com>
2013-10-25 15:24:16 +0300

    Add marker for unread outgoing messages Fixes #26

--- modules/chat/chat.popup.js
+++ modules/chat/chat.popup.js
@@ -1,21 +1,29 @@
 angular.module('chat', ['item', 'mediator', 'request', 'rectify'])
-    .controller('ChatCtrl', function ($scope, Mediator, Request) {
-        $scope.markAsRead = function (messages) {
+    .controller('ChatCtrl', function ($scope, Mediator, Request, $filter) {
+        var MARK_AS_READ = $filter('i18n')('Mark as read'),
+            YOUR_MESSAGE_WASNT_READ = $filter('i18n')('Your message wasn\'t read');
+
+        function markAsRead(messages) {
             Request.api({code: 'return API.messages.markAsRead({mids: ['
                 + _.pluck(messages, 'mid') + ']});'});
-        };
+        }
+
+        function showTooltip() {
+            jQuery(this).data('tooltip').$tip.remove();
+        }
 
         Mediator.pub('chat:data:get');
         Mediator.sub('chat:data', function (data) {
             $scope.$apply(function () {
                 $scope.data = data.dialogs.map(function (dialog) {
-                    var firstMessage = dialog.messages[0], result = {};
+                    var firstMessage = dialog.messages[0], result = {},
+                        selfProfile = _(data.profiles).findWhere({
+                            isSelf: true
+                        });
 
                     if (dialog.messages[0].out) {
                         // find logined user profile
-                        result.author = _(data.profiles).findWhere({
-                            isSelf: true
-                        });
+                        result.author = selfProfile;
                     } else if (dialog.chat_id) {
                         result.author = _(data.profiles).findWhere({
                             uid: firstMessage.uid
@@ -38,6 +46,16 @@ angular.module('chat', ['item', 'mediator', 'request', 'rectify'])
                     result.isUnread = dialog.messages[
                         dialog.messages.length - 1
                     ].read_state === 0;
+
+                    if (result.isUnread) {
+                        if (result.author === selfProfile) {
+                            result.unreadTooltip = YOUR_MESSAGE_WASNT_READ;
+                            result.unreadHandler = showTooltip;
+                        } else {
+                            result.unreadTooltip = MARK_AS_READ;
+                            result.unreadHandler = markAsRead;
+                        }
+                    }
 
                     return result;
                 });
