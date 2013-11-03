b457f05e2c7352caf8416b8790c64d28e5e203bc
Dmitriy Schadey <metrofun@dmitriys-air.dlink>
2013-11-04 01:40:43 +0200

    Too many comments to a post are possible Fixes #16

--- modules/news/news.popup.js
+++ modules/news/news.popup.js
@@ -1,4 +1,84 @@
 angular.module('news', ['mediator', 'navigation', 'rectify', 'request'])
+    .factory('News', function (Mediator) {
+        return {
+            unsubscribe: function (type, ownerId, itemId) {
+                var options = {
+                    type: type,
+                    owner_id: ownerId,
+                    item_id: itemId
+                };
+                Mediator.pub('feedbacks:unsubscribe', options);
+            },
+            getCommentsData: function (item) {
+                var comment, parent = item.parent, type;
+
+                switch (item.type) {
+                    case 'wall':
+                    case 'post':
+                    case 'mention':
+                        if (parent.comments.can_post) {
+                            comment = {
+                                ownerId: parent.owner_id,
+                                id: parent.id || parent.post_id,
+                                type: 'post'
+                            };
+                        }
+                        break;
+                    case 'comment':
+                        if (parent.post && parent.post.comments.can_post) {
+                            comment = {
+                                ownerId: parent.post.from_id,
+                                id: parent.post.id,
+                                replyTo: item.parent.id,
+                                type: 'post'
+                            };
+                        } else if (parent.topic && !parent.topic.is_closed) {
+                            comment = {
+                                ownerId: parent.topic.owner_id,
+                                id: parent.topic.tid,
+                                type: 'topic'
+                            };
+                        } else {
+                            if (parent.photo) {
+                                type = 'photo';
+                            } else if (parent.video) {
+                                type = 'video';
+                            }
+                            if (type) {
+                                comment = {
+                                    ownerId: parent[type].owner_id,
+                                    id: parent[type].id,
+                                    type: type
+                                };
+                            }
+                        }
+                        break;
+                    case 'topic':
+                        comment = {
+                            ownerId: parent.owner_id,
+                            id: parent.id || parent.post_id,
+                            type: 'topic'
+                        };
+                        break;
+                    case 'photo':
+                        comment = {
+                            ownerId: parent.owner_id,
+                            id: parent.pid,
+                            type: 'photo'
+                        };
+                        break;
+                    case 'video':
+                        comment = {
+                            ownerId: parent.owner_id,
+                            id: parent.id || parent.vid,
+                            type: 'video'
+                        };
+                        break;
+                }
+                return comment;
+            }
+        };
+    })
     .controller('NewsController', function ($scope, $routeParams) {
         $scope.subtabs = [
             {
@@ -16,90 +96,17 @@ angular.module('news', ['mediator', 'navigation', 'rectify', 'request'])
         ];
         $scope.activeSubTab = $routeParams.subtab;
     })
-    .controller('MyNewsController', function ($scope, Mediator) {
-        $scope.unsubscribe = function (type, ownerId, itemId) {
-            var options = {
-                type: type,
-                owner_id: ownerId,
-                item_id: itemId
-            };
-            Mediator.pub('feedbacks:unsubscribe', options);
-        };
-
+    .controller('MyNewsController', function ($scope, Mediator, News) {
+        $scope.unsubscribe = News.unsubscribe;
         Mediator.pub('feedbacks:data:get');
         Mediator.sub('feedbacks:data', function (data) {
             $scope.$apply(function () {
-                $scope.data = data;
-
                 if (data.items && data.items.length) {
                     data.items.forEach(function (item) {
-                        var comment, parent = item.parent, type;
-
-                        switch (item.type) {
-                        case 'wall':
-                        case 'post':
-                        case 'mention':
-                            if (parent.comments.can_post) {
-                                comment = {
-                                    ownerId: parent.owner_id,
-                                    id: parent.id || parent.post_id,
-                                    type: 'post'
-                                };
-                            }
-                            break;
-                        case 'comment':
-                            if (parent.post && parent.post.comments.can_post) {
-                                comment = {
-                                    ownerId: parent.post.from_id,
-                                    id: parent.post.id,
-                                    replyTo: item.parent.id,
-                                    type: 'post'
-                                };
-                            } else if (parent.topic && !parent.topic.is_closed) {
-                                comment = {
-                                    ownerId: parent.topic.owner_id,
-                                    id: parent.topic.tid,
-                                    type: 'topic'
-                                };
-                            } else {
-                                if (parent.photo) {
-                                    type = 'photo';
-                                } else if (parent.video) {
-                                    type = 'video';
-                                }
-                                if (type) {
-                                    comment = {
-                                        ownerId: parent[type].owner_id,
-                                        id: parent[type].id,
-                                        type: type
-                                    };
-                                }
-                            }
-                            break;
-                        case 'topic':
-                            comment = {
-                                ownerId: parent.owner_id,
-                                id: parent.id || parent.post_id,
-                                type: 'topic'
-                            };
-                            break;
-                        case 'photo':
-                            comment = {
-                                ownerId: parent.owner_id,
-                                id: parent.pid,
-                                type: 'photo'
-                            };
-                            break;
-                        case 'video':
-                            comment = {
-                                ownerId: parent.owner_id,
-                                id: parent.id || parent.vid,
-                                type: 'video'
-                            };
-                            break;
-                        }
-                        item.comment = comment;
+                        item.comment = News.getCommentsData(item);
                     });
+
+                    $scope.data = data;
                 }
             });
         });
