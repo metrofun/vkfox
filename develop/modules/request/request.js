59335ee1939d63fda499a22db1d1a80599494e50
Dmytrii Shchadei <gmetrofun@gmail.com>
2014-02-18 22:12:47 +0200

    v5.1.7 Grunt-browserify now uses unmodified prelude; Fixed wrong require

--- develop/modules/request/request.js
+++ develop/modules/request/request.js
@@ -5,7 +5,7 @@
 if (require('env/env.js').background) {
     module.exports = require('./request.bg.js');
 } else {
-    var ProxyMethods = require(('proxy-methods/proxy-methods.js'));
+    var ProxyMethods = require('proxy-methods/proxy-methods.js');
 
     module.exports = ProxyMethods.forward('request/request.bg.js', ['api', 'post', 'get']);
 }
--- develop/modules/users/users.pu.js
+++ develop/modules/users/users.pu.js
@@ -1,3 +1,3 @@
-var ProxyMethods = require(('proxy-methods/proxy-methods.js'));
+var ProxyMethods = require('proxy-methods/proxy-methods.js');
 
 module.exports = ProxyMethods.forward('users/users.bg.js', ['getProfilesById']);
