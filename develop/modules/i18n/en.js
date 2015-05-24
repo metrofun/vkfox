75266149098ef14b99449c282f9299b386e7e61f
Dmytrii Shchadei <gmetrofun@gmail.com>
2015-05-25 00:21:23 +0200

    make working for Chrome + search engine override WIP

--- develop/modules/i18n/en.js
+++ develop/modules/i18n/en.js
@@ -1,334 +1,69 @@
 (function(){ module.exports || (module.exports = {}) 
 var MessageFormat = { locale: {} };
-MessageFormat.locale.en = function ( n ) {
-  if ( n === 1 ) {
-    return "one";
-  }
-  return "other";
-};
+MessageFormat.locale.en=function(n){return n===1?"one":"other"}
 
 module.exports["en"] = {}
-module.exports["en"]["chat"] = function(d){
-var r = "";
-r += "chat";
-return r;
-}
-module.exports["en"]["news"] = function(d){
-var r = "";
-r += "news";
-return r;
-}
-module.exports["en"]["buddies"] = function(d){
-var r = "";
-r += "buddies";
-return r;
-}
-module.exports["en"]["my"] = function(d){
-var r = "";
-r += "my";
-return r;
-}
-module.exports["en"]["friends_nominative"] = function(d){
-var r = "";
-r += "friends";
-return r;
-}
-module.exports["en"]["groups_nominative"] = function(d){
-var r = "";
-r += "groups";
-return r;
-}
-module.exports["en"]["Private message"] = function(d){
-var r = "";
-r += "Private message";
-return r;
-}
-module.exports["en"]["Wall post"] = function(d){
-var r = "";
-r += "Wall post";
-return r;
-}
-module.exports["en"]["Search"] = function(d){
-var r = "";
-r += "First or last name";
-return r;
-}
-module.exports["en"]["Male"] = function(d){
-var r = "";
-r += "Male";
-return r;
-}
-module.exports["en"]["Female"] = function(d){
-var r = "";
-r += "Female";
-return r;
-}
-module.exports["en"]["Offline"] = function(d){
-var r = "";
-r += "Offline";
-return r;
-}
-module.exports["en"]["Bookmarked"] = function(d){
-var r = "";
-r += "Bookmarked";
-return r;
-}
-module.exports["en"]["Monitor online status"] = function(d){
-var r = "";
-r += "Monitor online status";
-return r;
-}
-module.exports["en"]["Mark as read"] = function(d){
-var r = "";
-r += "Mark as read";
-return r;
-}
-module.exports["en"]["Your message wasn't read"] = function(d){
-var r = "";
-r += "Your message wasn't read";
-return r;
-}
-module.exports["en"]["Like"] = function(d){
-var r = "";
-r += "Like";
-return r;
-}
-module.exports["en"]["Show history"] = function(d){
-var r = "";
-r += "Show history";
-return r;
-}
-module.exports["en"]["Open in New Tab"] = function(d){
-var r = "";
-r += "Open in New Tab";
-return r;
-}
-module.exports["en"]["unsubscribe"] = function(d){
-var r = "";
-r += "unsubscribe";
-return r;
-}
-module.exports["en"]["more..."] = function(d){
-var r = "";
-r += "more";
-return r;
-}
-module.exports["en"]["Comment"] = function(d){
-var r = "";
-r += "Comment";
-return r;
-}
-module.exports["en"]["Liked"] = function(d){
-var r = "";
-r += "Liked";
-return r;
-}
-module.exports["en"]["Reposted"] = function(d){
-var r = "";
-r += "Reposted";
-return r;
-}
-module.exports["en"]["New friends:"] = function(d){
-var r = "";
-r += "New friends:";
-return r;
-}
-module.exports["en"]["started following you"] = function(d){
-var r = "";
-r += "started following you";
-return r;
-}
-module.exports["en"]["friend request accepted"] = function(d){
-var r = "";
-r += "friend request accepted";
-return r;
-}
-module.exports["en"]["sent a message"] = function(d){
-var r = "";
-if(!d){
-throw new Error("MessageFormat: No data passed to function.");
-}
-r += d["NAME"];
-r += " sent a message";
-return r;
-}
-module.exports["en"]["is online"] = function(d){
-var r = "";
-r += "is online";
-return r;
-}
-module.exports["en"]["is_online_short"] = function(d){
-var r = "";
-r += "appeared";
-return r;
-}
-module.exports["en"]["went offline"] = function(d){
-var r = "";
-r += "went offline";
-return r;
-}
-module.exports["en"]["went_offline_short"] = function(d){
-var r = "";
-r += "went";
-return r;
-}
-module.exports["en"]["left a comment"] = function(d){
-var r = "";
-if(!d){
-throw new Error("MessageFormat: No data passed to function.");
-}
-r += d["NAME"];
-r += " left a comment";
-return r;
-}
-module.exports["en"]["mentioned you"] = function(d){
-var r = "";
-r += "mentioned you";
-return r;
-}
-module.exports["en"]["posted on your wall"] = function(d){
-var r = "";
-r += "posted on your wall";
-return r;
-}
-module.exports["en"]["liked your comment"] = function(d){
-var r = "";
-r += "liked your comment";
-return r;
-}
-module.exports["en"]["liked your post"] = function(d){
-var r = "";
-r += "liked your post";
-return r;
-}
-module.exports["en"]["liked your photo"] = function(d){
-var r = "";
-r += "liked your photo";
-return r;
-}
-module.exports["en"]["liked your video"] = function(d){
-var r = "";
-r += "liked your video";
-return r;
-}
-module.exports["en"]["shared your post"] = function(d){
-var r = "";
-r += "shared your post";
-return r;
-}
-module.exports["en"]["shared your photo"] = function(d){
-var r = "";
-r += "shared your photo";
-return r;
-}
-module.exports["en"]["shared your video"] = function(d){
-var r = "";
-r += "shared your video";
-return r;
-}
-module.exports["en"]["notifications"] = function(d){
-var r = "";
-r += "notifications";
-return r;
-}
-module.exports["en"]["force online"] = function(d){
-var r = "";
-r += "Be always online";
-return r;
-}
-module.exports["en"]["sound"] = function(d){
-var r = "";
-r += "sound";
-return r;
-}
-module.exports["en"]["signal"] = function(d){
-var r = "";
-r += "signal";
-return r;
-}
-module.exports["en"]["volume"] = function(d){
-var r = "";
-r += "volume";
-return r;
-}
-module.exports["en"]["popups"] = function(d){
-var r = "";
-r += "popups";
-return r;
-}
-module.exports["en"]["show text"] = function(d){
-var r = "";
-r += "show message text";
-return r;
-}
-module.exports["en"]["show all"] = function(d){
-var r = "";
-r += "show all";
-return r;
-}
-module.exports["en"]["hide"] = function(d){
-var r = "";
-r += "show less";
-return r;
-}
-module.exports["en"]["Yandex search"] = function(d){
-var r = "";
-r += "Yandex search";
-return r;
-}
-module.exports["en"]["install_noun"] = function(d){
-var r = "";
-r += "install";
-return r;
-}
-module.exports["en"]["install_verb"] = function(d){
-var r = "";
-r += "install";
-return r;
-}
-module.exports["en"]["skip"] = function(d){
-var r = "";
-r += "skip";
-return r;
-}
-module.exports["en"]["login"] = function(d){
-var r = "";
-r += "login";
-return r;
-}
-module.exports["en"]["accept"] = function(d){
-var r = "";
-r += "accept";
-return r;
-}
-module.exports["en"]["no"] = function(d){
-var r = "";
-r += "no";
-return r;
-}
-module.exports["en"]["close"] = function(d){
-var r = "";
-r += "close";
-return r;
-}
-module.exports["en"]["Authorize VKfox with Vkontakte"] = function(d){
-var r = "";
-r += "First you need to authorize VKfox to connect with VK.COM™. If you are doing this for the first time you will be asked to grant access to your account.";
-return r;
-}
-module.exports["en"]["Accept license agreement"] = function(d){
-var r = "";
-r += "By installing this application you agree to all terms, conditions, and information of the <a anchor='http://vkfox.io/license'>license agreement.</a>";
-return r;
-}
-module.exports["en"]["Install Yandex search"] = function(d){
-var r = "";
-r += "Please consider supporting future development of VKfox by installing Yandex search.";
-return r;
-}
-module.exports["en"]["Thank you!"] = function(d){
-var r = "";
-r += "Thank you, installation is complete! Now this window can be closed.";
-return r;
-}
+module.exports["en"]["chat"] = function(d){return "chat"}
+module.exports["en"]["news"] = function(d){return "news"}
+module.exports["en"]["buddies"] = function(d){return "buddies"}
+module.exports["en"]["my"] = function(d){return "my"}
+module.exports["en"]["friends_nominative"] = function(d){return "friends"}
+module.exports["en"]["groups_nominative"] = function(d){return "groups"}
+module.exports["en"]["Private message"] = function(d){return "Private message"}
+module.exports["en"]["Wall post"] = function(d){return "Wall post"}
+module.exports["en"]["Search"] = function(d){return "First or last name"}
+module.exports["en"]["Male"] = function(d){return "Male"}
+module.exports["en"]["Female"] = function(d){return "Female"}
+module.exports["en"]["Offline"] = function(d){return "Offline"}
+module.exports["en"]["Bookmarked"] = function(d){return "Bookmarked"}
+module.exports["en"]["Monitor online status"] = function(d){return "Monitor online status"}
+module.exports["en"]["Mark as read"] = function(d){return "Mark as read"}
+module.exports["en"]["Your message wasn't read"] = function(d){return "Your message wasn't read"}
+module.exports["en"]["Like"] = function(d){return "Like"}
+module.exports["en"]["Show history"] = function(d){return "Show history"}
+module.exports["en"]["Open in New Tab"] = function(d){return "Open in New Tab"}
+module.exports["en"]["unsubscribe"] = function(d){return "unsubscribe"}
+module.exports["en"]["more..."] = function(d){return "more"}
+module.exports["en"]["Comment"] = function(d){return "Comment"}
+module.exports["en"]["Liked"] = function(d){return "Liked"}
+module.exports["en"]["Reposted"] = function(d){return "Reposted"}
+module.exports["en"]["New friends:"] = function(d){return "New friends:"}
+module.exports["en"]["started following you"] = function(d){return "started following you"}
+module.exports["en"]["friend request accepted"] = function(d){return "friend request accepted"}
+module.exports["en"]["sent a message"] = function(d){return v(d,"NAME")+" sent a message"}
+module.exports["en"]["is online"] = function(d){return "is online"}
+module.exports["en"]["is_online_short"] = function(d){return "appeared"}
+module.exports["en"]["went offline"] = function(d){return "went offline"}
+module.exports["en"]["went_offline_short"] = function(d){return "went"}
+module.exports["en"]["left a comment"] = function(d){return v(d,"NAME")+" left a comment"}
+module.exports["en"]["mentioned you"] = function(d){return "mentioned you"}
+module.exports["en"]["posted on your wall"] = function(d){return "posted on your wall"}
+module.exports["en"]["liked your comment"] = function(d){return "liked your comment"}
+module.exports["en"]["liked your post"] = function(d){return "liked your post"}
+module.exports["en"]["liked your photo"] = function(d){return "liked your photo"}
+module.exports["en"]["liked your video"] = function(d){return "liked your video"}
+module.exports["en"]["shared your post"] = function(d){return "shared your post"}
+module.exports["en"]["shared your photo"] = function(d){return "shared your photo"}
+module.exports["en"]["shared your video"] = function(d){return "shared your video"}
+module.exports["en"]["notifications"] = function(d){return "notifications"}
+module.exports["en"]["force online"] = function(d){return "Be always online"}
+module.exports["en"]["sound"] = function(d){return "sound"}
+module.exports["en"]["signal"] = function(d){return "signal"}
+module.exports["en"]["volume"] = function(d){return "volume"}
+module.exports["en"]["popups"] = function(d){return "popups"}
+module.exports["en"]["show text"] = function(d){return "show message text"}
+module.exports["en"]["show all"] = function(d){return "show all"}
+module.exports["en"]["hide"] = function(d){return "show less"}
+module.exports["en"]["Yandex search"] = function(d){return "Yandex search"}
+module.exports["en"]["install_noun"] = function(d){return "install"}
+module.exports["en"]["install_verb"] = function(d){return "install"}
+module.exports["en"]["skip"] = function(d){return "skip"}
+module.exports["en"]["login"] = function(d){return "login"}
+module.exports["en"]["accept"] = function(d){return "accept"}
+module.exports["en"]["no"] = function(d){return "no"}
+module.exports["en"]["close"] = function(d){return "close"}
+module.exports["en"]["Authorize VKfox with Vkontakte"] = function(d){return "First you need to authorize VKfox to connect with VK.COM™. If you are doing this for the first time you will be asked to grant access to your account."}
+module.exports["en"]["Accept license agreement"] = function(d){return "By installing this application you agree to all terms, conditions, and information of the <a anchor='http://vkfox.io/license'>license agreement.</a>"}
+module.exports["en"]["Install Yandex search"] = function(d){return "Please consider supporting future development of VKfox by installing Yandex search."}
+module.exports["en"]["Thank you!"] = function(d){return "Thank you, installation is complete! Now this window can be closed."}
 })();
